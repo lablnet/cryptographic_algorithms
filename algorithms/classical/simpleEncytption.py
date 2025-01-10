ce34b8b989469ff88482466a345b82996f43c116
Muhammad Umer Farooq <umer@lablnet.com>
2025-01-10 06:48:17 +0500

    Enhance simple encryption algorithm with validation and documentation
    - Added docstring to describe the encode_decode function parameters and return value.
    - Implemented assertions to validate input types for text, operation type, and key.
    - Refactored the main execution block to demonstrate encoding and decoding with example output.

--- algorithms/classical/simpleEncytption.py
+++ algorithms/classical/simpleEncytption.py
@@ -1,21 +1,37 @@
-text = "Hello World"
 def encode_decode(text, type="encode", key=1):
-    assert type in ["encode", "decode"]
-    assert key in range(1, 255)
+    """
+    Simple encryption and decryption algorithm.
+    :param text: str, the text to encode or decode.
+    :param type: str, the type of operation to perform.
+    :param key: int, the key to use for the operation.
+    :return: str, the encoded or decoded text.
+    """
     
+    # check if the type is valid
+    assert type in ["encode", "decode"]
+    # check if the key is valid
+    assert key in range(1, 255)
+    # check if the text is valid
+    assert isinstance(text, str)
+
     t = ""
     for char in text:
+        # convert char to ascii
         a = ord(char)
         if type == "encode":
+            # add key to ascii
             a += key
         elif type == "decode":
+            # subtract key from ascii
             a -= key
-        # a = a % 26
         # convert back to char
         b = chr(a)
-        t += b    
+        t += b
     return t
 
-encode = encode_decode(text, "encode", 1)
-decode = encode_decode(encode, "decode", 1)
-print(text, encode, decode)
+if __name__ == "__main__":
+    text = "Hello World"
+    key = 254
+    encode = encode_decode(text, "encode", key) # output: "Ifmmp Xpsme"
+    decode = encode_decode(encode, "decode", key) # output: "Hello World"
+    print("text: ", text, "\nencode: ", encode, "\ndecode: ", decode)
